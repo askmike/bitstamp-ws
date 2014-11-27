2eec72b5e12c766fcf6b6392d17c64e8de376493
dsri <dkbsri@gmail.com>
2014-11-27 13:57:10 -0500

    add orderbook channels

--- example.js
+++ example.js
@@ -4,3 +4,9 @@ var ws = new Bitstamp();
 ws.on('trade', function(trade) {
   console.log('new trade:', trade);
 });
+ws.on('order_book', function(data) {
+  console.log('new order book:', data);
+});
+ws.on('diff_order_book', function(data) {
+  console.log('new order book diff:', data);
+});
