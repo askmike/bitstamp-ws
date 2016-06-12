c3e75a1b24df7b70ff63b99ff0583cd608e67198
Mike van Rossum <mike@mikevanrossum.nl>
2016-06-12 16:08:22 +0200

    update example

--- example.js
+++ example.js
@@ -6,8 +6,8 @@ var ws = new Bitstamp({
 
   // BTC/USD market:
   live_trades: true,
-  order_book: false,
-  diff_order_book: false,
+  order_book: true,
+  diff_order_book: true,
 
   // BTC/EUR market:
   live_trades_btceur: false,
@@ -15,25 +15,18 @@ var ws = new Bitstamp({
   diff_order_book_btceur: false
 });
 
-console.log('listening to new trades..');
+// if you enabeld `live_trades` or `live_trades_btceur`
 
+console.log('listening to new trades..');
 ws.on('trade', function(trade) {
   console.log('new trade:', trade);
 });
 
 
-// if you enabeld `order_book` and/or `diff_order_book`
+// if you enabeld `order_book`, `diff_order_book`,
+// `order_book_btceur` and/or `diff_order_book_btceur`.
 
+console.log('listening to new order book events..');
 ws.on('data', function(data) {
   console.log('new order book event:', data);
 });
-
-
-// if you enabeld `live_trades_btceur`, `order_book_btceur` and/or `diff_order_book_btceur`
-
-// ws.on('trade', function(data) {
-//   console.log('new trade:', data);
-// });
-// ws.on('data', function(data) {
-//   console.log('new order book event:', data);
-// });
