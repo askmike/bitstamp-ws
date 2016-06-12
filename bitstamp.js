044e3ede4a5de66702b739b24edb4dd8a999b7ae
Mike van Rossum <mike@mikevanrossum.nl>
2016-06-12 16:00:01 +0200

    add eur market, limit event listening to once per type

--- bitstamp.js
+++ bitstamp.js
@@ -1,4 +1,3 @@
-// var Pusher = require('pusher-node-client').PusherClient;
 var Pusher = require('pusher-js/node');
 
 var BITSTAMP_PUSHER_KEY = 'de504dc5763aeef9ff52';
@@ -20,6 +19,14 @@ var Bitstamp = function(opts) {
       encrypted: this.opts.encrypted
   });
 
+
+  // bitstamp publishes all data over just 2 channels
+  // make sure we only subscribe to each channel once
+  this.bound = {
+    trade: false,
+    data: false
+  }
+
   this.subscribe();
 }
 
@@ -28,27 +35,50 @@ var EventEmitter = require('events').EventEmitter;
 util.inherits(Bitstamp, EventEmitter);
 
 Bitstamp.prototype.subscribe = function() {
+
+  // BTC/USD events
   if(this.opts.live_trades) {
+    console.log('sub trades')
     this.client.subscribe('live_trades');
     this.client.bind('trade', this.broadcast('trade'));
   }
   if(this.opts.order_book) {
+    console.log('sub order_book')
     this.client.subscribe('order_book');
-    this.client.bind('data', this.broadcast('order_book'));
+    this.client.bind('data', this.broadcast('data'));
   }
   if(this.opts.diff_order_book) {
+    console.log('sub diff_order_book')
     this.client.subscribe('diff_order_book');
-    this.client.bind('data', this.broadcast('diff_order_book'));
+    this.client.bind('data', this.broadcast('data'));
+  }
+
+  // BTC/EUR events
+  if(this.opts.live_trades_btceur) {
+    this.client.subscribe('live_trades_btceur');
+    this.client.bind('trade', this.broadcast('trade'));
+  }
+  if(this.opts.order_book_btceur) {
+    this.client.subscribe('order_book_btceur');
+    this.client.bind('data', this.broadcast('data'));
+  }
+  if(this.opts.diff_order_book_btceur) {
+    this.client.subscribe('diff_order_book_btceur');
+    this.client.bind('data', this.broadcast('data'));
   }
 };
 
 Bitstamp.prototype.broadcast = function(name) {
+  if(this.bound[name])
+    return function noop() {};
+
+  console.log('routing', name);
+
+  this.bound[name] = true;
+
   return function(e) {
     this.emit(name, e)
   }.bind(this);
 };
-
-
-// new Bitstamp;
 
 module.exports = Bitstamp;
--- example.js
+++ example.js
@@ -1,10 +1,18 @@
 var Bitstamp = require('./Bitstamp');
 
 var ws = new Bitstamp({
+  // force encrypted socket session
   encrypted: true,
+
+  // BTC/USD market:
   live_trades: true,
   order_book: false,
-  diff_order_book: false
+  diff_order_book: false,
+
+  // BTC/EUR market:
+  live_trades_btceur: false,
+  order_book_btceur: false,
+  diff_order_book_btceur: false
 });
 
 console.log('listening to new trades..');
@@ -14,11 +22,18 @@ ws.on('trade', function(trade) {
 });
 
 
-// if you enabeld order_book and/or diff_order_book
+// if you enabeld `order_book` and/or `diff_order_book`
 
-// ws.on('order_book', function(data) {
-//   console.log('new order book:', data);
+ws.on('data', function(data) {
+  console.log('new order book event:', data);
+});
+
+
+// if you enabeld `live_trades_btceur`, `order_book_btceur` and/or `diff_order_book_btceur`
+
+// ws.on('trade', function(data) {
+//   console.log('new trade:', data);
 // });
-// ws.on('diff_order_book', function(data) {
-//   console.log('new order book diff:', data);
+// ws.on('data', function(data) {
+//   console.log('new order book event:', data);
 // });
